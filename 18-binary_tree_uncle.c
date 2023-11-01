31a702f0a0bc40063cc425dd20f46fefee9b5160
Kevin Warui <waruimacharia@gmail.com>
2023-11-01 09:08:26 -0700

    Refactor 18-binary_tree_uncle.c

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -11,20 +11,19 @@
 
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	binary_tree_t *g_parent;
-
 	if (!node || !node->parent || !node->parent->parent)
 		return (NULL);
 
-	g_parent = node->parent->parent;
-
-	if (g_parent->left->left == node || g_parent->left->right == node)
-		if (g_parent->right)
-			return (g_parent->right);
-
-	if (g_parent->right->left == node || g_parent->right->right == node)
-		if (g_parent->left)
-			return (g_parent->left);
+	if (node->parent->parent->left == node->parent)
+	{
+		if (node->parent->parent->right)
+			return (node->parent->parent->right);
+	}
+	else
+	{
+		if (node->parent->parent->left)
+			return (node->parent->parent->left);
+	}
 
 	return (NULL);
 }
