902ab9bea4d5172b3da3d4c6eaa7ad1c080a1324
Kevin Warui <waruimacharia@gmail.com>
2023-11-01 04:09:13 -0700

    Refactor 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -48,11 +48,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	if (!tree)
+		return (0);
 	if (binary_tree_balance(tree->left) < 0)
 		return (0);
 	if (binary_tree_balance(tree->right) < 0)
 		return (0);
-	if (binary_tree_balance(tree) >= 0)
-		return (1);
-	return (0);
+	if (binary_tree_balance(tree) < 0)
+		return (0);
+	return (1);
 }
